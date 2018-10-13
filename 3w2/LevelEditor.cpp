523fb4b52743268b8ce6b68eb702c2e32c12c251
ChrAlb <albchr@gmail.com>
2018-10-13 18:20:22 +0200

    included LevelEditor Header and ccp

---
+++ 3w2/LevelEditor.cpp
@@ -0,0 +1,32 @@
+#include "stdafx.h"
+#include "LevelEditor.h"
+
+void LevelEditor::OnCreate()
+{
+	;
+}
+
+void LevelEditor::OnDestroy()
+{
+	;
+}
+
+void LevelEditor::Activate()
+{
+	;
+}
+
+void LevelEditor::Deactivate()
+{
+	;
+}
+
+void LevelEditor::Update(const sf::Time & l_time)
+{
+	;
+}
+
+void LevelEditor::Draw()
+{
+	;
+}
---
+++ 3w2/LevelEditor.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <SFML\Graphics.hpp>
+#include "BaseState.h"
+#include <string>
+
+using namespace sf;
+
+class LevelEditor : public BaseState
+{
+public:
+
+	void OnCreate();
+	void OnDestroy();
+
+	void Activate();
+	void Deactivate();
+
+	void Update(const sf::Time& l_time);
+	void Draw();
+
+
+private:
+
+	const int m_NumberofFiles;
+	
+	Vector2i   m_LevelDimension;
+	View       m_TileView;
+	View       m_DesignView;
+	View       m_LevelViel;
+
+	String     m_tileFile;
+	String     m_LevelFile;
+
+	VertexArray  m_TileArray;
+	VertexArray  m_LevelArray;  
+
+};
