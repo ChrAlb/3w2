9df4472edeaf71a5ab6c864774488630bea9ed2e
ChrAlb <albchr@gmail.com>
2019-01-27 17:11:55 +0100

    working on level manager

--- 3w2/LevelManager.h
+++ 3w2/LevelManager.h
@@ -3,6 +3,7 @@
 #include <SFML/Graphics.hpp>
 #include "SharedContext.h"
 #include "ResourceIdentifiers.h"
+#include <vector>
 
 using namespace sf;
 using namespace std;
@@ -12,28 +13,35 @@ struct leveldate
 	string BGFileName;
 	string TilSetName;
 	string LevelName;
-	int MaxInt;
+	
 	Textures::ID m_bg_texture;
 	Textures::ID m_tileset;
 
+	Vector2f m_PlayerStartPosition;
+	Vector2f m_EnenemyStartPosition;
+
+    //int MaxInt;
 };
 
  
 class LevelManager
 {
 private:
+	
+	int m_NumofLevels;
 	Vector2i m_LevelSize;
-	Vector2f m_StartPosition;
+	
 	float m_TimeModifier = 1;
 	int m_CurrentLevel = 0;
-	const int NUM_LEVELS = 2;
+	
 	bool m_allLevel = false;
 	
 	leveldate m_leveldata;
 
+	vector<leveldate> m_levelinfo;
 	
-	
-	
+	//Vector2f m_StartPosition;
+   //const int NUM_LEVELS = 2;
 
 public:
 
